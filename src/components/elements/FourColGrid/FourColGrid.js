f949ce99783f257f3e8f0168177334c1b10b4a44
Pedro Sardinha <psardinha@pdmfc.com>
2019-09-26 13:51:44 +0100

    first commit

---
+++ src/components/elements/FourColGrid/FourColGrid.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FourColGrid.css'
+
+const FourColGrid = (props) => {
+
+return (
+    <div>four col grid</div>
+)
+
+}
+
+export default FourColGrid
---
+++ src/components/elements/SearchBar/SearchBar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import FontAwesome from 'react-fontawesome'
+import './SearchBar.css'
+
+class SearchBar extends React.Component {
+
+    state = {
+
+    }
+
+    render () {
+        return (
+            <div>searchbar</div>
+        )
+    }
+}
+
+export default SearchBar
