52af757803c57c3f4fdf64ce2805cb49c1fc9af7
Pedro Sardinha <psardinha@pdmfc.com>
2019-09-30 16:52:53 +0100

    routing and actors finished

---
+++ src/components/elements/NotFound/NoutFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+
+    return(
+    <div>
+        <h1>Huuummm.. Seems like the page you are looking for doesn't exist..</h1>
+    </div>
+    )
+}
+
+export default NotFound
